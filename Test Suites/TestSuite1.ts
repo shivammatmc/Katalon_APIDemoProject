8c16bf98702a87cda13ae70b98a779a3300d7e94
IT 76 <IT 76@IT76>
2020-09-21 13:57:26 +0530

    second project commit

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>26d988f0-d333-499a-b7c2-07c9b5fffdd7</testSuiteGuid>
+   <testCaseLink>
+      <guid>f8a9c2d7-86a4-4442-b571-753260b4c550</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CalculatorTests/Addition</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>75cf0384-67a0-4320-a099-18ac8a038ec9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CalculatorTests/ListUsersTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e5d7db7c-2983-49f1-8574-5521cb1cac5c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GetBankDetails</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>03c4c178-86db-4709-9fcf-4446875dd77a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bb2afda8-19ac-47a6-939e-3459818e9b3d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestHttpMethods</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
